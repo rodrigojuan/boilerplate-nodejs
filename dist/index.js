984c17593cee2141f9f75ea2834c1024507afb94
Rodrigo Saes Juan <rodrigo.juan@gmail.com>
2024-03-31 11:38:44 -0300

    chore: configure initial api project

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express();
+app.get('/', (req, res) => {
+    console.log('executado no servidor');
+    return res.json({ message: 'hello world' });
+});
+app.listen(3333, () => {
+    console.log('server is running on port 3333');
+});
